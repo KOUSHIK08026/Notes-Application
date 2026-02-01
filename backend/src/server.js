7892592b58f1c76f3d9108745caac48d619bec49
KOUSHIK08026 <sarkarkoushik2006@gmail.com>
2026-02-01 13:51:44 +0530

    Basic cards added

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors"
 dotenv.config();
 
 const app=express();
@@ -13,8 +14,16 @@ const PORT=process.env.PORT || 5001
 
 
 // middleware
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+    }
+));
 app.use(express.json());
 app.use(rateLimiter);
+
+
+
 app.use((req, res, next) => {
   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
   next();
