a3e80f1a810ef0727b3d40a5f5727a532dbd6a87
KOUSHIK08026 <sarkarkoushik2006@gmail.com>
2026-02-01 09:35:04 +0530

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+import express from "express"
+//for using this change the type to module in package.json
+//const express=require("express");
+
+import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv"
+import rateLimiter from "./middleware/rateLimiter.js";
+dotenv.config();
+
+const app=express();
+const PORT=process.env.PORT || 5001
+
+
+// middleware
+app.use(express.json());
+app.use(rateLimiter);
+app.use((req, res, next) => {
+  console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+  next();
+});
+
+
+app.use(express.json());
+app.use("/api/notes",notesRoutes)
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
